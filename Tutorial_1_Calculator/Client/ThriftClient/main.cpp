3fe20b023c9c9291e491546ea4367b942413cf65
qtuancr261 <tuancr261@gmail.com>
2018-07-10 16:23:00 +0700

    include some headers, output main's parameters at the beginning

--- Tutorial_1_Calculator/Client/ThriftClient/main.cpp
+++ Tutorial_1_Calculator/Client/ThriftClient/main.cpp
@@ -1,6 +1,10 @@
-
+#include <iostream>
+#include <thrift/protocol/TBinaryProtocol.h>
+using namespace std;
 int main(int argc, char *argv[])
 {
+    for (int index{}; index < argc; ++index)
+        cout << argv[index] << endl;
 
     return 0;
 }
