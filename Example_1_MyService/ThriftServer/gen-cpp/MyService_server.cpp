349ff5679c39929dd71ac006cace07a4288a9367
qtuancr261 <tuancr261@gmail.com>
2018-07-11 17:51:24 +0700

    Update Thrift Server code 	- Support TNonblockingServer

--- Example_1_MyService/ThriftServer/gen-cpp/MyService_server.cpp
+++ Example_1_MyService/ThriftServer/gen-cpp/MyService_server.cpp
@@ -6,12 +6,15 @@
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/server/TNonblockingServer.h>
+#include <thrift/concurrency/PosixThreadFactory.h>
+#include <thrift/concurrency/ThreadManager.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
-
+using namespace ::apache::thrift::concurrency;
 using boost::shared_ptr;
 
 using namespace  ::ZTE;
@@ -41,14 +44,27 @@ class MyServiceHandler : virtual public MyServiceIf {
 
 int main(int argc, char **argv) {
   int port = 9090;
-  shared_ptr<MyServiceHandler> handler(new MyServiceHandler());
+  /*shared_ptr<MyServiceHandler> handler(new MyServiceHandler());
   shared_ptr<TProcessor> processor(new MyServiceProcessor(handler));
   shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
   shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
   shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
   TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-  server.serve();
+  server.serve();*/
+  shared_ptr<MyServiceHandler> handler(new MyServiceHandler());
+  shared_ptr<TProcessor> processor(new MyServiceProcessor(handler));
+  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+
+      // using thread pool with maximum 15 threads to handle incoming requests
+      shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager(15);
+      shared_ptr<PosixThreadFactory> threadFactory = shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
+      threadManager->threadFactory(threadFactory);
+      threadManager->start();
+      TNonblockingServer server(processor, protocolFactory, 8888, threadManager);
+      server.serve();
+
+
   return 0;
 }
 
